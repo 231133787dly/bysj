48e466b0ff25925de6ac3eb13803590ace2b856f
231133787dly <231133787@qq.com>
2021-03-23 15:47:24 +0800

    first

---
+++ src/main/java/com/controller/InsertEssentialData.java
@@ -0,0 +1,20 @@
+package com.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping(value = "/hello")
+public class InsertEssentialData {
+    @RequestMapping(value = "/helloworld")
+    public String helloWorld(){
+        log.info("helloworld!");
+        return "/index";
+    }
+
+}
+
